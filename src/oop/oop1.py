914cb1da247bc920d7fd9a47fe8a6dec5d3ac29b
hyetigran <tigran.asriyan@gmail.com>
2020-02-14 11:15:09 -0500

    oop1 file complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,84 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# class Vehicle:
+#     def __init__(self, name):
+#         self.name = name
+
+
+# class GroundVehicle(Vehicle):
+#     def __init__(self, name, typeV):
+#         super().__init__(name)
+#         self.typeV = typeV
+
+
+# class FlightVehicle(Vehicle):
+#     def __init__(self, name, typeV):
+#         super().__init__(name)
+#         self.typeV = typeV
+
+
+# class Car(GroundVehicle):
+#     def __init__(self, name, typeV, axle):
+#         super().__init__(name, typeV)
+#         self.axle = axle
+
+
+# class MotorCycle(GroundVehicle):
+#     def __init__(self, name, typeV, throttle):
+#         super().__init__(name, typeV)
+#         self.throttle = throttle
+
+
+# class Airplane(FlightVehicle):
+#     def __init__(self, name, typeV, wings):
+#         super().__init__(name, typeV)
+#         self.wings = wings
+
+
+# class Starship(FlightVehicle):
+#     def __init__(self, name, typeV, thrust):
+#         super().__init__(name, typeV)
+#         self.thrust = thrust
+
+class Vehicle:
+    def __init__(self):
+        pass
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
